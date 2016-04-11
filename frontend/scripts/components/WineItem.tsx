1e7cb8f29f88366d48566a3422d39e2d3754c5f4
Suchan Lee <suchanl@palantir.com>
2016-04-10 22:47:16 -0700

    New styles for wine item

--- frontend/scripts/components/WineItem.tsx
+++ frontend/scripts/components/WineItem.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
+import * as Constants from '../constants/Constants';
 
 const QUOTE_WORD_LIMIT: number = 15;
+
+interface WineName {
+    winery: string;
+    rest: string;
+}
 
 interface Props {
     wine: Types.Wine;
@@ -27,6 +34,14 @@ export default class WineItem extends React.Component<Props, State> {
             }
             return this.props.wine.quote;
         }
+    }
+
+    private getWineName(): WineName {
+        const nameSplit = this.props.wine.name.split(',');
+        return {
+            winery: nameSplit[0],
+            rest: nameSplit.slice(1).join(', ')
+        };
     }
 
     private renderCircles(circleClass: string, count: number): React.ReactElement<any>[] {
@@ -60,29 +75,39 @@ export default class WineItem extends React.Component<Props, State> {
 
     render() {
         const wine: Types.Wine = this.props.wine;
-        const backgroundStyle = {
-            'backgroundImage': `url('https://${wine.alt_image_url}')`
-        };
+        const wineName: WineName = this.getWineName();
         return (
             <li className='wine-item'>
-                <div className='wine-item-image' style={backgroundStyle} alt={wine.name} />
-                <div className='wine-item-info-container'>
-                    <div className='wine-item-info-wrapper'>
+                <div className="wine-type-container">
+                    <div className={classNames('wine-type-box', {
+                        'red': wine.wine_type.toUpperCase() === Constants.WineType.RED,
+                        'white': wine.wine_type.toUpperCase() === Constants.WineType.WHITE,
+                        'rose': wine.wine_type.toUpperCase() === Constants.WineType.ROSE,
+                        'sparkling': wine.wine_type.toUpperCase() === Constants.WineType.SPARKLING
+                    })}></div>
+                </div>
+                <div className="wine-item-container">
+                    <div className='wine-item-info-container'>
                         <div className='wine-item-info'>
-                            <h2>{wine.name}</h2>
+                            <div className="wine-name-container">
+                                <div className='wine-name'>{wineName.winery}</div>
+                                <div className='wine-name'>{wineName.rest}</div>
+                            </div>
                             <div>{`${wine.subregion}, ${wine.region} ${wine.vintage}`}</div>
-                            <div>{wine.varietal}</div>
-                            <table className="ratings">
-                                <tbody>
-                                    {this.renderRating('fruit', wine.fruit_rating)}
-                                    {this.renderRating('tannin', wine.tannin_rating)}
-                                    {this.renderRating('earth', wine.earth_rating)}
-                                    {this.renderRating('acid', wine.acid_rating)}
-                                    {this.renderRating('body', wine.body_rating)}
-                                    {this.renderRating('alcohol', wine.alcohol_rating)}
-                                </tbody>
-                            </table>
+                            <div className='varietal'>{wine.varietal}</div>
                         </div>
+                    </div>
+                    <div className="wine-item-ratings-container">
+                        <table className="ratings">
+                            <tbody>
+                                {this.renderRating('Fruit', wine.fruit_rating)}
+                                {this.renderRating('Tannin', wine.tannin_rating)}
+                                {this.renderRating('Earth', wine.earth_rating)}
+                                {this.renderRating('Acid', wine.acid_rating)}
+                                {this.renderRating('Body', wine.body_rating)}
+                                {this.renderRating('Alcohol', wine.alcohol_rating)}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </li>
--- frontend/scripts/constants/Constants.ts
+++ frontend/scripts/constants/Constants.ts
@@ -1,1 +1,8 @@
 export const WineTypes = ['red', 'white', 'rose', 'sparkling'];
+
+export const WineType = {
+    'RED': 'RED',
+    'WHITE': 'WHITE',
+    'ROSE': 'ROSE',
+    'SPARKLING': 'SPARKLING'
+};
