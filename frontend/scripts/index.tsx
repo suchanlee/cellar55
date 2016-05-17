a35b1fa3cf0da8243b210bdfe93a8f40ee483ab6
Suchan Lee <suchanl@palantir.com>
2016-05-16 21:25:05 -0700

    update typings to match API for typings 1.0+

--- frontend/scripts/index.tsx
+++ frontend/scripts/index.tsx
@@ -4,18 +4,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router';
-import * as ReactRouterRedux from 'react-router-redux';
 
 import { initialState } from './initialState';
 import cellarStore from './stores/cellarStore';
 import App from './components/App.tsx';
 
 const store = cellarStore(initialState);
-const history = ReactRouterRedux.syncHistoryWithStore(browserHistory, store) as any;
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
+        <Router history={browserHistory}>
             <Route path='/' component={App}>
                 <Route path='wine/:wineId' component={App} />
             </Route>
--- frontend/scripts/stores/cellarStore.ts
+++ frontend/scripts/stores/cellarStore.ts
@@ -1,18 +1,18 @@
 import { combineReducers, createStore, applyMiddleware, IMiddleware, IStore } from 'redux';
 import * as thunkMiddleware from 'redux-thunk';
-import * as ReactRouterRedux from 'react-router-redux';
+
+import { IApp } from '../types/main';
 
 import wineReducer from '../reducers/wineReducer';
 import filterReducer from '../reducers/filterReducer';
 
 const rootReducer = combineReducers({
     wine: wineReducer,
-    filter: filterReducer,
-    routing: ReactRouterRedux.routerReducer
+    filter: filterReducer
 });
 
 const enhancers = applyMiddleware(thunkMiddleware['default']);
 
-export default function cellarStore(initialState: Types.IApp): IStore<Types.IApp> {
+export default function cellarStore(initialState: IApp): IStore<IApp> {
     return createStore(rootReducer, enhancers);
 }
