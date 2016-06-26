e7414554250120a065c6c041de4821b13bbc01da
Suchan Lee <suchanl@palantir.com>
2016-06-25 22:20:28 -0700

    support loading config from yml file

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+import yaml
+from os import environ, path
+
+from flask import Flask as BaseFlask, Config as BaseConfig
 
 from flask_sqlalchemy import SQLAlchemy
 
-from config import DevelopmentConfig
+# yaml-based config snipper from: https://gist.github.com/mattupstate/2046115
+################################################
+class Config(BaseConfig):
+    """Flask config enhanced with a `from_yaml` method."""
+
+    def from_yaml(self, config_file):
+        env = environ.get('FLASK_ENV', 'development')
+        self['ENVIRONMENT'] = env.lower()
+
+        with open(config_file) as f:
+            c = yaml.load(f)
+
+        c = c.get(env, c)
+
+        for key in c.iterkeys():
+            if key.isupper():
+                self[key] = c[key]
+
+class Flask(BaseFlask):
+    """Extended version of `Flask` that implements custom config class"""
+
+    def make_config(self, instance_relative=False):
+        root_path = self.root_path
+        if instance_relative:
+            root_path = self.instance_path
+        return Config(root_path, self.default_config)
+
+################################################
+
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app.config.from_yaml(path.join(app.root_path, "conf", "config.yml"))
 db = SQLAlchemy(app)
