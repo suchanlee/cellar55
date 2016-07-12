5efbb42ee9f60eeb9c6e4f490615adedc3829305
Suchan Lee <suchanl@palantir.com>
2016-07-11 23:19:04 -0700

    move scroll to contain both body and footer of filter

--- cellar55/frontend/scripts/components/filters/FilterContainer.tsx
+++ cellar55/frontend/scripts/components/filters/FilterContainer.tsx
@@ -18,13 +18,15 @@ export default class FilterContainer extends React.Component<Props, void> {
                 <div className="filter-header">
                     <h1>CELLAR 55</h1>
                 </div>
-                <div className="filter-body">{this.props.children}</div>
-                <div className="filter-footer">
-                    <ul>
-                        <li><a href="https://sommselect.com" target="_blank">Sommselect</a></li>
-                        <li>Terms</li>
-                        <li>Privacy</li>
-                    </ul>
+                <div className="filter-scrollable">
+                    <div className="filter-body">{this.props.children}</div>
+                    <div className="filter-footer">
+                        <ul>
+                            <li><a href="https://sommselect.com" target="_blank">Sommselect</a></li>
+                            <li>Terms</li>
+                            <li>Privacy</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
